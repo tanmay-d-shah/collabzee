33341d5dc61259e8173d62bf6ea69eb1836ab4ac
Tanmay Shah <tanmayshah@Tanmays-MacBook-Air.local>
2025-02-08 20:13:01 +0530

    feat: init collabzee backend server

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import express, { Application, Request, Response } from 'express';
+
+const app: Application = express();
+const port: number = 3000;
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello World with TypeScript!');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
